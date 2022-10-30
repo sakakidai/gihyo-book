f26d862f13867d7c2b53530eaf65ad5459adf222
sakakidai <sd.lybd1016@gmail.com>
2022-10-30 13:29:01 +0900

    prettierとeslintの設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -31,15 +31,15 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta key="charset" name="charset" content="utf-8" />
+        <meta key='charset' name='charset' content='utf-8' />
         {/* width=device-widthはデフォルトでinitial-scale=1となる */}
         <meta
-          key="viewport"
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+          key='viewport'
+          name='viewport'
+          content='width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5'
         />
-        <meta property="og:locale" content="ja_JP" />
-        <meta property="og:type" content="website" />
+        <meta property='og:locale' content='ja_JP' />
+        <meta property='og:type' content='website' />
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
